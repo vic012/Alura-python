98a2739b46f0a0b5d8d797ffe3536742f374a77a
Pedro Henrique <henriquevic012@gmail.com>
2021-03-19 18:04:03 -0300

    Aprendendo Encapsulamento

--- conta.py
+++ conta.py
@@ -19,3 +19,32 @@ class Conta:
 
 	def depositar(self, valor):
 		self.__saldo += valor
+
+	def transferir(self, valor, destino):
+		#Para usar esse método preciso ter duas contas cadastradas no mínimo
+
+		'''Execução no CMD
+		caixa = Conta(135, 'Zé', 50, 1000)
+		inter = Conta(527, 'Asis', 200, 1000)
+
+		caixa.transferir(50, caixa, inter)
+		Aqui eu repeti caixa, vams melhorar isso
+
+		caixa.extrato()
+		O saldo da conta de Zé é de R$ 100
+		inter.extrato()
+		O saldo da conta de Asis é de R$ 150
+
+		podemos deixa no atributo da seguinte forma:
+		def transferir(self, valor, destino) em vez de
+		def transferir(self, valor, origem, destino) 
+
+		dessa forma, em vez de:
+
+		origem.sacar(valor)
+		destino.depositar(valor)
+
+		Fica:'''
+
+		self.sacar(valor)
+		destino.depositar(valor)
